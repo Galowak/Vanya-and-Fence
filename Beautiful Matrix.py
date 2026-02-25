2c5ee9c3f9ba7d880909d51964399112cbb898f0
Galowak <gelanawakbeka@gmail.com>
2026-02-25 11:08:28 +0300

    Create Beautiful Matrix.py

---
+++ Beautiful Matrix.py
@@ -0,0 +1,10 @@
+def beautiful_matrix(matrix):
+    for i in range(5):
+        for j in range(5):
+            if matrix[i][j] == 1:
+                return abs(i - 2) + abs(j - 2)
+matrix = []
+for _ in range(5):
+    row = list(map(int, raw_input().split()))
+    matrix.append(row)
+print(beautiful_matrix(matrix))
